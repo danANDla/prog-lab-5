f8dfb4f1b234f1aef8a14fb55a5394f41d87f3ab
archdla <d.a.n.202021@gmail.com>
2022-01-13 23:31:08 +0300

    updateBand

--- src/main/java/collection/utils/CollectionManager.java
+++ src/main/java/collection/utils/CollectionManager.java
@@ -1,7 +1,12 @@
 package collection.utils;
 
+import collection.music.Album;
+import collection.music.Coordinates;
 import collection.music.MusicBand;
+import collection.music.MusicGenre;
+import exceptions.InvalidIdException;
 
+import java.util.Date;
 import java.util.LinkedHashSet;
 
 import static java.lang.Math.min;
@@ -36,6 +41,125 @@ public class CollectionManager {
             badId+=1;
         }
         return badId;
+    }
+
+    private MusicBand getBandById(Integer id) throws InvalidIdException{
+        boolean found = false;
+        for(MusicBand band: bandsList){
+            if(band.getId() == id){
+                return band;
+            }
+        }
+        if(!found) throw new InvalidIdException();
+        return null;
+    }
+
+    public boolean updateBand(Integer idUpdate, String field){
+        field = field.toUpperCase();
+        try {
+            MusicBand foundBand = getBandById(idUpdate);
+            String newName = asker.askName();
+            if(foundBand!=null) foundBand.setName(newName);
+        }
+        catch (InvalidIdException e){
+            return false;
+        }
+        switch (field){
+            case "NAME":{ // todo check for askers
+                try {
+                    MusicBand foundBand = getBandById(idUpdate);
+                    String newName = asker.askName();
+                    if(foundBand!=null) foundBand.setName(newName);
+                    return true;
+                }
+                catch (InvalidIdException e){
+                    return false; // todo messgae invalid id
+                }
+            }
+            case "COORDINATES":{
+                try {
+                    MusicBand foundBand = getBandById(idUpdate);
+                    Coordinates newCoordinates = asker.askCoordinates();
+                    if(foundBand!=null) foundBand.setCoordinates(newCoordinates);
+                    return true;
+                }
+                catch (InvalidIdException e){
+                    return false; // todo messgae invalid id
+                }
+            }
+            case "CREATION_DATE":{
+                try {
+                    MusicBand foundBand = getBandById(idUpdate);
+                    Date newDate = asker.askDate();
+                    if(foundBand!=null) foundBand.setCreationDate(newDate);
+                    return true;
+                }
+                catch (InvalidIdException e){
+                    return false; // todo messgae invalid id
+                }
+            }
+            case "PARTICIPANTS_NUM":{
+                try {
+                    MusicBand foundBand = getBandById(idUpdate);
+                    long newParticipants = asker.askParticipants();
+                    if(foundBand!=null) foundBand.setNumberOfParticipants(newParticipants);
+                    return true;
+                }
+                catch (InvalidIdException e){
+                    return false; // todo messgae invalid id
+                }
+            }
+            case "ALBUMS_COUNT":{
+                try {
+                    MusicBand foundBand = getBandById(idUpdate);
+                    int newAlbums = asker.askAlbums();
+                    if(foundBand!=null) foundBand.setAlbumsCount(newAlbums);
+                    return true;
+                }
+                catch (InvalidIdException e){
+                    return false; // todo messgae invalid id
+                }
+            }
+            case "DESCRIPTION":{
+                try {
+                    MusicBand foundBand = getBandById(idUpdate);
+                    String newDescription = asker.askDescription();
+                    if(foundBand!=null) foundBand.setDescription(newDescription);
+                    return true;
+                }
+                catch (InvalidIdException e){
+                    return false; // todo messgae invalid id
+                }
+
+            }
+            case "GENRE":{
+                try {
+                    MusicBand foundBand = getBandById(idUpdate);
+                    MusicGenre newGenre = asker.askGenre();
+                    if(foundBand!=null) foundBand.setGenre(newGenre);
+                    return true;
+                }
+                catch (InvalidIdException e){
+                    return false; // todo messgae invalid id
+                }
+
+            }
+            case "BEST_ALBUM":{
+                try {
+                    MusicBand foundBand = getBandById(idUpdate);
+                    Album newBest = asker.askBest();
+                    if(foundBand!=null) foundBand.setBestAlbum(newBest);
+                    return true;
+                }
+                catch (InvalidIdException e){
+                    return false; // todo messgae invalid id
+                }
+
+            }
+            default:{
+                return false; // todo message invalid field
+            }
+        }
     }
 
     public boolean removeBand(Integer idRemove){
--- src/main/java/collection/utils/MusicBandFactory.java
+++ src/main/java/collection/utils/MusicBandFactory.java
@@ -1,87 +1,21 @@
 package collection.utils;
 
-import collection.music.Album;
-import collection.music.Coordinates;
 import collection.music.MusicBand;
-import collection.music.MusicGenre;
 
-import java.util.Date;
+
 
 
 public class MusicBandFactory {
-    private Integer id = 0;
-
     public MusicBand makeBand(){
         return new MusicBand(
-                askName(),
-                askCoordinates(),
-                askDate(),
-                askParticipants(),
-                askAlbums(),
-                askDescription(),
-                askGenre(),
-                askBest()
+                asker.askName(),
+                asker.askCoordinates(),
+                asker.askDate(),
+                asker.askParticipants(),
+                asker.askAlbums(),
+                asker.askDescription(),
+                asker.askGenre(),
+                asker.askBest()
         );
     }
-
-    public Integer getId(){
-        id++;
-        return id;
-    }
-
-    private String askName(){
-        String bandName;
-        bandName = ""; // todo ask name
-        return bandName;
-    }
-
-    private Coordinates askCoordinates(){
-       Coordinates bandCoordinates = new Coordinates();
-       double x = 0; // todo ask for coordinates
-       int y = 0;
-       bandCoordinates.setX(x);
-       bandCoordinates.setY(y);
-       return bandCoordinates;
-    }
-
-    private Date askDate(){
-        int day=1;
-        int month=2;
-        int year=1970;
-        Date creationDate = new Date();
-        creationDate.setYear(year); // todo ask for Date
-        creationDate.setMonth(month);
-        creationDate.setDate(day);
-        return creationDate;
-    }
-
-    private long askParticipants(){
-        long participantsNumber = 0; // todo ask for participants number
-        return participantsNumber;
-    }
-
-    private int askAlbums(){
-        int albumsNumber = 0; // todo ask for albums number
-        return albumsNumber;
-    }
-
-    private String askDescription(){
-        String bandDescription = ""; // todo ask for description
-        return bandDescription;
-    }
-
-    private MusicGenre askGenre(){
-        MusicGenre bandGenre = MusicGenre.BLUES; // todo ask for genre
-        return bandGenre;
-    }
-
-    private Album askBest(){
-        String albumName = "Piano"; // todo ask for best album
-        int tracksNumber = 2;
-        Integer albumLength = null;
-        double albumSales = 2.3;
-        Album bestAlbum = new Album(albumName, tracksNumber, albumLength, albumSales);
-        return bestAlbum;
-    }
-
 }
