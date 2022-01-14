253183c880a4b240c6a5b0d73c1b439e8ee3a937
archdla <d.a.n.202021@gmail.com>
2022-01-14 15:47:28 +0300

    history command

--- src/main/java/commands/CommandsManager.java
+++ src/main/java/commands/CommandsManager.java
@@ -3,6 +3,7 @@ package commands;
 import collection.utils.CollectionManager;
 import services.IOutil;
 
+import java.util.ArrayDeque;
 import java.util.HashMap;
 import java.util.Locale;
 
@@ -12,9 +13,13 @@ public class CommandsManager {
     private IOutil io;
     CollectionManager collectionManager;
 
+    ArrayDeque<String> history;
+
     public CommandsManager(IOutil ioutil){
         io = ioutil;
         collectionManager = new CollectionManager(io);
+        history = new ArrayDeque<String>();
+
         fillLists();
     }
 
@@ -30,6 +35,7 @@ public class CommandsManager {
         argumentedComandsList.put("update", new Update(collectionManager, io));
         argumentedComandsList.put("remove_by_id", new RemoveById(collectionManager,io));
         commandsList.put("clear", new Clear(collectionManager));
+        commandsList.put("history", new History(history, io));
     }
 
     public void executeCommand(String newCommand){
@@ -37,10 +43,18 @@ public class CommandsManager {
         if(argumentedComandsList.containsKey(command[0])){
             ArgumentedCommand parsedCommand = argumentedComandsList.get(command[0]);
             if (parsedCommand.parseArgs(command)) parsedCommand.execute();
+            history.addFirst(command[0]);
+            if(history.size() > 14){
+                history.removeLast();
+            }
         }
         else if(commandsList.containsKey(command[0])){
             Command parsedCommand = commandsList.get(command[0]);
             parsedCommand.execute();
+            history.addFirst(command[0]);
+            if(history.size() > 14){
+                history.removeLast();
+            }
         }
         else{
             io.printError("Такой команды не найдено");
