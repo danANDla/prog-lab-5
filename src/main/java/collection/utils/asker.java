e3119d52e974cee206ddefa4059ec2f2972e9ce3
archdla <d.a.n.202021@gmail.com>
2022-01-14 12:25:30 +0300

    added left functions in asker

--- src/main/java/collection/utils/asker.java
+++ src/main/java/collection/utils/asker.java
@@ -15,7 +15,7 @@ public class asker {
        String str;
        str = io.readLine().trim();
        while(str.equals("")){
-           io.printError("обязательно поле");
+           io.printError("Это обязательно поле");
            str = io.readLine();
        }
        return str;
@@ -23,7 +23,11 @@ public class asker {
 
     private static void printGenres(){
         io.printText("список жанров:");
-
+        io.printText("  (1) Progressive rock");
+        io.printText("  (2) Jazz");
+        io.printText("  (3) Blues");
+        io.printText("  (4) Pop");
+        io.printText("  (5) Math rock");
     }
 
     public static String askName(){
@@ -117,6 +121,10 @@ public class asker {
             try {
                 participantsNumber = Long.parseLong(getNonEmpty());
                 valid = true;
+                if(participantsNumber <= 0){
+                    io.printError("Количество участников должно быть больше 0");
+                    valid = false;
+                }
             } catch (NumberFormatException e){
                 io.printError("Неправильный формат целого числа (long)");
             }
@@ -132,6 +140,10 @@ public class asker {
             try{
                 albumsNumber = Integer.parseInt(getNonEmpty());
                 valid = true;
+                if(albumsNumber <= 0){
+                    io.printError("Количество альбомов должно быть больше 0");
+                    valid = false;
+                }
             } catch (NumberFormatException e){
                 io.printError("Неправильный формат целого числа");
             }
@@ -147,19 +159,133 @@ public class asker {
     }
 
     public static MusicGenre askGenre(){
-        io.printText("Введите музыкальный жанр");
-        io.printText("доступные жанры:");
+        boolean valid = false;
+        int intGenre = -1;
+        MusicGenre bandGenre = null;
+        io.printText("Введите музыкальный жанр (строка или число 1-5)");
         printGenres();
-        MusicGenre bandGenre = MusicGenre.BLUES; // todo ask for genre
+        while(!valid){
+            String str = getNonEmpty();
+            str = str.toUpperCase();
+            switch (str){
+                case "PROGRESSIVE ROCK":
+                    intGenre = 1;
+                    break;
+                case "BLUES":
+                    intGenre = 2;
+                    break;
+                case "JAZZ":
+                    intGenre = 3;
+                    break;
+                case "POP":
+                    intGenre = 4;
+                    break;
+                case "MATH ROCK":
+                    intGenre = 5;
+                default:{
+                    try{
+                        intGenre = Integer.parseInt(str);
+                    } catch (NumberFormatException e){
+                        //io.printError("Неправильный формат ввода");
+                    }
+                }
+            }
+            switch(intGenre){
+                case 1:
+                    bandGenre = MusicGenre.PROGRESSIVE_ROCK;
+                    valid = true;
+                    break;
+                case 2:
+                    bandGenre = MusicGenre.JAZZ;
+                    valid = true;
+                    break;
+                case 3:
+                    bandGenre = MusicGenre.BLUES;
+                    valid = true;
+                    break;
+                case 4:
+                    bandGenre = MusicGenre.POP;
+                    valid = true;
+                    break;
+                case 5:
+                    bandGenre = MusicGenre.MATH_ROCK;
+                    valid = true;
+                    break;
+                default:
+                    io.printError("Неправильный формат ввода");
+            }
+        }
         return bandGenre;
     }
 
     public static Album askBest(){
-        String albumName = "Piano"; // todo ask for best album
-        int tracksNumber = 2;
-        Integer albumLength = null;
-        double albumSales = 2.3;
-        Album bestAlbum = new Album(albumName, tracksNumber, albumLength, albumSales);
-        return bestAlbum;
+        io.printText("Введите данные о лучшем альбоме");
+        return new Album(askAlbumName(), askTracksNumber(), askAlbumLength(), askAlbumSales());
     }
+    private static String askAlbumName(){
+        io.printText("название альбома");
+        String albumName = getNonEmpty();
+        return albumName;
+    }
+    private static int askTracksNumber(){
+        io.printText("количество треков в альбоме");
+        int tracksNumber = -1;
+        boolean valid = false;
+        while(!valid){
+            try{
+                tracksNumber = Integer.parseInt(getNonEmpty());
+                valid = true;
+                if(tracksNumber <= 0){
+                    io.printError("Количество треков должно быть больше 0");
+                    valid = false;
+                }
+            } catch (NumberFormatException e){
+                io.printError("Неправильный формат целого числа");
+            }
+        }
+        return tracksNumber;
+    }
+    private static Integer askAlbumLength(){
+        io.printText("продолжительность альбома");
+        String str;
+        int albumLength = -1;
+        boolean valid = false;
+        while (!valid){
+            str = io.readLine().trim();
+            if(str.equals("")) return null;
+            try{
+                albumLength = Integer.parseInt(getNonEmpty());
+                valid = true;
+                if(albumLength <= 0){
+                    io.printError("Продолжительность должна быть быть больше 0");
+                    valid = false;
+                }
+            } catch (NumberFormatException e){
+                io.printError("Неправильный формат целого числа");
+            }
+        }
+        return albumLength;
+    }
+    private static double askAlbumSales(){
+        io.printText("продажи альбома (вещественное число)");
+        String str;
+        double albumSales = -1;
+        boolean valid = false;
+        while (!valid){
+            str = io.readLine().trim();
+            if(str.equals("")) return -1;
+            try{
+                albumSales = Double.parseDouble(str);
+                valid = true;
+                if(albumSales <= 0){
+                    io.printError("продажи должны быть быть больше 0");
+                    valid = false;
+                }
+            } catch (NumberFormatException e){
+                io.printError("Неправильный формат вещественного числа");
+            }
+        }
+        return albumSales;
+    }
+
 }
